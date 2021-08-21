d23d90a8b59e03f8e7cda87a68a8282adcc6b5f2
s1957154 <s1957154@g.tohoku-gakuin.ac.jp>
2021-08-21 12:23:13 +0900

    commit firebase setting

--- build/app/generated/source/buildConfig/debug/com/example/sumple2/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/sumple2/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.sumple2;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.sumple2";
+  public static final String APPLICATION_ID = "com.rush_mhr.sumple2";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
